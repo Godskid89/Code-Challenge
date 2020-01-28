55befd1d5c046c295e5d6b13965a8c9c1130cf03
Joseph Oladokun <josepholadokun2@gmail.com>
2020-01-28 18:48:17 +0100

    readme and viz

--- code.R
+++ code.R
@@ -1,50 +1,61 @@
-install.packages("jsonlite")
+#install packages
+install.packages("jsonlite", "dplyr", "ggplot2", "reshape2", "magrittr", "leaflet", "forcats", "tidyr", "devtools", "htmlwidgets")
 
+#Import Library
 library(jsonlite)
 library(dplyr)
 library(ggplot2)
 library(reshape2)
 library(magrittr)
 library(leaflet)
-library(googleway)
+library(forcats)
+library(tidyr)
+library(htmlwidgets)
 
+#Import Data
 full_data <- jsonlite::fromJSON("https://feeds.citibikenyc.com/stations/stations.json")
-
 crashes <- jsonlite::fromJSON("https://data.cityofnewyork.us/resource/h9gi-nx95.json")
 
+#Pull out stations data
 stations <- full_data$stationBeanList
 
-str(crashes)
-
+#Change Data type
 crashes <- crashes %>% mutate_at(vars(matches("number")), funs(as.numeric))
 
-
+#Create a new column adding the number of people affected in bike crash
 df <- mutate(crashes, total_affected_cyclist = (number_of_cyclist_killed + number_of_cyclist_injured))
 
-# Most Dangerous borough
 
-ggplot(data = df, aes(x = borough, y = total_affected_cyclist)) +
-  geom_bar(stat = "identity")
+# Visualizing the most most dangerous borough
+cyclist_crashed <- ggplot(df, aes(x=borough, y=total_affected_cyclist)) + 
+                      geom_bar(stat = "identity", fill="#f68060", alpha =.6, width = .4) +
+                      coord_flip()+
+                      xlab("") +
+                      theme_bw() +
+                      ggtitle("Total Number of Cyclist involved in Bike crash in New York")
+
+#Save vizualization
+ggsave("cyclist_crashed.png")
 
 
+# Create a color palette
+mybins <- seq(0, 72, by=15)
+mypalette <- colorBin(palette = "YlOrBr", domain=stations$availableBikes, na.color="transparent", bins=mybins)
 
-mybins <- seq(0, 78, by 10)
-mypalette <- colorBin(palette = "YlorBr", domain=stations$availableDocks, na.color="transparent", bins=mybins)
-
-mytext <- paste(
-  "Station Name: ", stations$stationName, "<br/>",
-  "Total Docks: ", stations$totalDocks, "<br/>"
-) %>%
+# Prepare the tooltip
+mytext <- paste("Station Name: ", stations$stationName) %>% 
   lapply(htmltools::HTML)
 
+# Map visualization
 map <- leaflet(stations) %>%
   addTiles() %>%
   addProviderTiles("Esri.WorldImagery")%>%
   addCircleMarkers(~longitude, ~latitude,
-                   fillColor = ~mypalette(availableDocks), fillOpacity = 0.7, color = "white", radius = 8, stroke = FALSE,
+                   fillColor = ~mypalette(availableBikes), fillOpacity = 0.7, color = "white", radius = 5, stroke = FALSE,
                    label = mytext,
                    labelOptions = labelOptions(style = list("font-weight"="normal", padding = "3px 8px"), textsize = "13px", direction = "auto")
                    ) %>%
-  addLegend(pal = mypalette, values = ~availableDocks, opacity = 0.9, title = "Magnitude", position = "bottomright")
+  addLegend(pal = mypalette, values = ~availableBikes, opacity = 0.9, title = "Number of Available bikes", position = "bottomright")
 
-map
+#Save visualization
+saveWidget(map, "bikes_available.html", selfcontained = FALSE)
---
+++ readme1.md
@@ -0,0 +1,15 @@
+# Code Challenge
+
+This is the repo for solution to the code challenge
+
+## Setting Up
+
+To set up the environment for this analysis follow the following steps
+
+- clone the repository ```git clone https://github.com/Godskid89/Code-Challenge.git ``` and open it in R Studio
+
+- Set working directory to the project location i.e Session > Set Working Directory > To project Directory
+- in the console, type ```source("code.R")``` and run it
+- Two files will be created
+    - HTML file for map visualization of showing the number of available bikes in each location
+    - A jpg visualization showing the total number of Cyclist affected by accidents
